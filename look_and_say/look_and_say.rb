60066cc9713890bbce53e3d91bd2bc339bcf1ae7
Ian Anderson <ianderson@academicsoftwareplus.com>
2011-10-06 10:33:35 -0400

    solution is a bit more concise now

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -1,5 +1,5 @@
 class LookAndSay
-  
+
   def initialize(string_or_array)
     string = (string_or_array.is_a? Array) ? string_or_array.join : string_or_array
     # digits only
@@ -9,17 +9,13 @@ class LookAndSay
 
   def next
     same_digit_counter = 0
-    i = 0
     next_string = ""
-    loop do
-      break if @current[i].nil?
-
+    @current.split(//).each_index do |i|
       same_digit_counter += 1
       if @current[i] != @current[i+1]
         next_string << "#{same_digit_counter}#{@current[i]}"
         same_digit_counter = 0
       end
-      i +=1
     end
     @current = next_string
     self
