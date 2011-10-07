1caba008b2149caa351c32940164ebaba0d01f3e
Long Nguyen <long.nguyen11288@gmail.com>
2011-10-07 14:38:09 -0400

    finished look and say

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -1,15 +1,23 @@
+
 class LookAndSay
-  
+   
   def initialize(string)
-    # Implement me!
+    if(string.kind_of?(Array))
+      @user_input = string.each { |x| x.to_s }   
+    elsif(string.to_i.to_s == string)
+      @user_input = string
+    else
+      raise ArgumentError, "Show not allow strings with non-digit characters"
+    end
   end
 
   def next
-    # Implement me!
+    @user_input = @user_input.gsub(/(.)\1*/) {|s|  s.size.to_s + s[0]}    
+    self
   end
 
   def to_s
-    # Implement me!
+    @user_input
   end
   
 end
